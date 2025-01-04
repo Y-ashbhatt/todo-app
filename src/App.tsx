cc43902eeaea559d5ee689d812772585539fd7d1
Yash Bhatt <30y.bhatt@gmail.com>
2025-01-04 18:47:40 +0530

    Adding tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,57 @@
+import React from 'react';
+
+type Priority = 'p1' | 'p2' | 'p3';
+
+type Task = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+  priority?: Priority;
+};
+
 function App() {
-  return <div>Hello World!</div>;
+  const [taskName, setTaskName] = React.useState('');
+
+  const [tasks, setTasks] = React.useState<Task[]>([
+    {
+      id: 1,
+      title: 'Learn React',
+      isCompleted: true,
+      priority: 'p1',
+    },
+  ]);
+
+  const onAddTask = () => {
+    setTasks([
+      ...tasks,
+      {
+        id: new Date().getTime(), // Not a great way to generate IDs
+        title: taskName,
+        isCompleted: false,
+      },
+    ]);
+  };
+
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <label htmlFor="task-input">Add Task: </label>
+      <input
+        id="task-input"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+      />
+      <button onClick={onAddTask}>Add</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <input type="checkbox" checked={task.isCompleted} />
+            {task.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
